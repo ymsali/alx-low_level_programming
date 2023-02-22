01b36f574413d972e8be088b0708fc2aae9e1458
ymsali <ymsalis@gmail.com>
2023-02-22 06:24:13 -0800

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - inlowercase
+ * Return:0
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z' letter++)
+	{
+		_putchar(letter);
+
+	}
+	_putchar('\n');
+
+}
