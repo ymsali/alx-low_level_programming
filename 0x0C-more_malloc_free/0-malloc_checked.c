a778d9a651d795d83d72db71dd657e85fa6ccdb5
ymsali <ymsalis@gmail.com>
2023-03-16 15:20:53 -0700

    task0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -10,11 +10,10 @@
 
 void *malloc_checked(unsigned int b)
 {
-	void *ptr;
-	ptr = malloc(b);
+	int *m = malloc(b);
 
-	if (ptr == NULL)
+	if (m == 0)
 		exit(98);
 
-	return (ptr);
+	return (m);
 }
