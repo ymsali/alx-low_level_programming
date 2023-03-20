0e820898cd0f8ae969dec9b1fe84cbd6e7158ede
ymsali <ymsalis@gmail.com>
2023-03-20 09:58:16 -0700

    correction task

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,25 +1,20 @@
 #include "dog.h"
+#include <stdlib.h>
 #include <stdio.h>
-
 /**
- * print_dog - Prints a struct dog
- *
- * @d: pointer to d dog
- *
- * Return: Pointer to struct
+ * print_dog - print a structure.
+ * @d: pointer containing the structure.
  */
-
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-	if (d->name == NULL)
-		printf("Name: (nil)\n");
-	else
-		printf("Name: %s\n", d->name);
-	printf("Age: %f\n", d->age);
-	if (d->owner == NULL)
-		printf("Owner: (nil)\n");
-	else
-		printf("Owner: %s\n", d->owner);
+	if ((*d).name == NULL)
+		(*d).name = "(nil)";
+
+	if ((*d).owner == NULL)
+		(*d).owner = "(nil)";
+	printf("Name: %s\n", (*d).name);
+	printf("Age: %f\n", (*d).age);
+	printf("Owner: %s\n", (*d).owner);
 }
--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,58 +1,38 @@
 #include "dog.h"
-#include <stdlib.h>
 /**
- * new_dog -  creates a new dog.
- *
- *
- * @name: char type
- *
- * @age: float type
- *
- * @owner: char type
- *
- * Return: Pointer to struct
- */
-
+  * new_dog - creates a new dog
+  * @name: new dog's name
+  * @age: new dog's age
+  * @owner: new dog's owner
+  * Return: pointer to new dog struct
+**/
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *newDog;
-	int length1, length2, i, j;
+	dog_t *new_dog;
+	int i;
 
-	length1 = 0, length2 = 0;
-	newDog = malloc(sizeof(struct dog));
-
-	if (newDog == NULL)
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
 		return (NULL);
-
-	while (name[length1++])
-		;
-	while (owner[length2++])
-		;
-
-	newDog->name = malloc(sizeof(newDog->name) * length1);
-
-	if (newDog->name == NULL)
+	new_dog->name = malloc(sizeof(name) + 1);
+	if (new_dog->name == NULL)
 	{
-		free(newDog);
+		free(new_dog);
 		return (NULL);
 	}
-
-	for (i = 0; i < length1; i++)
-		newDog->name[i] = name[i];
-
-	newDog->age = age;
-
-
-	newDog->owner = malloc(sizeof(newDog->owner) * length2);
-
-	if (newDog->owner == NULL)
+	new_dog->owner = malloc(sizeof(owner) + 1);
+	if (new_dog->owner == NULL)
 	{
-		free(newDog->name);
-		free(newDog);
+		free(new_dog->name);
+		free(new_dog);
 		return (NULL);
 	}
-	for (j = 0; j < length2; j++)
-		newDog->owner[j] = owner[j];
-
-	return (newDog);
+	for (i = 0; name[i]; i++)
+		new_dog->name[i] = name[i];
+	new_dog->name[i] = '\0';
+	new_dog->age = age;
+	for (i = 0; owner[i]; i++)
+		new_dog->owner[i] = owner[i];
+	new_dog->owner[i] = '\0';
+	return (new_dog);
 }
