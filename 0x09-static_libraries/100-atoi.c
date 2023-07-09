60afa43be080cb3bc4303da8a46ecf4904c2a3bb
ymsali <ymsalis@gmail.com>
2023-07-09 13:31:39 -0700

    hardwork commit

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,45 +1,21 @@
-#include <stdio.h>
-
 /**
- * _atoi - a function that converts a string to an integer
- * @s: An input string
- *
- * Description: This function converts a string to an integer.
- * The number in the string can be preceded by an infinite number of characters.
- * It takes into account all the '-' and '+' signs before the number.
- * If there are no numbers in the string, the function returns 0.
- * The function does not use the 'long' type or declare new variables of "type" array.
- * The code is compiled with the '-fsanitize=signed-integer-overflow' flag.
- * Note: The standard library provides a similar function: atoi.
- *
- * Return: The integer from conversion
- */
+* _atoi - changes a string to an int
+* @s: the string to be changed
+*
+* Return: the converted int
+*/
 int _atoi(char *s)
 {
-	int sign = 1;
-	unsigned int total = 0;
-	char null_flag = 0;
-
-	while (*s)
-	{
+	int i = 1;
+	unsigned int num = 0;
+	do {
 		if (*s == '-')
-			sign *= -1;
+		i *= -1;
 
-		if (*s >= '0' && *s <= '9')
-		{
-			null_flag = 1;
-			total = total * 10 + (*s - '0');
-		}
-		else if (null_flag)
-		{
-			break;
-		}
-		s++;
-	}
-
-	if (sign < 0)
-		total = -total;
-
-	return total;
+		else if (*s >= '0' && *s <= '9')
+		num = num * 10 + (*s - '0');
+		else if (num > 0)
+		     break;
+	} while (*s++);
+		return (num *i);
 }
-
